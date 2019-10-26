8d66997e841e5f037e28d81921051757059ca423
Vinay V <cool00geek@yahoo.com>
2019-10-26 12:06:55 -0700

    Get everything related to OCR working

--- app/src/main/java/com/sdhacks/flyerscanner/MainActivity.java
+++ app/src/main/java/com/sdhacks/flyerscanner/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +31,9 @@ import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -38,49 +47,146 @@ public class MainActivity extends AppCompatActivity {
     private static final boolean QUANT = true;
     private static final String LABEL_PATH = "labels.txt";
     private static final int INPUT_SIZE = 224;
+    private static final int RESULT_LOAD_IMAGE = 29;
+    private static final int RESULT_TAKE_PHOTO=13;
 
     private Classifier classifier;
     private Executor executor = Executors.newSingleThreadExecutor();
 
     private GraphicOverlay mGraphicOverlay;
+    private String currentPhotoPath;
+
+    private String mCurrentPhotoPath;
+    private Bitmap help1;
+
+    private Uri file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initTensorFlowAndLoadModel();
-        dispatchTakePictureIntent();
+        requestPermissionForReadExtertalStorage();
     }
 
     private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        /*Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+        */
+
+
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //file = Uri.fromFile(getFile());
+        //cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,file);
+        //if(cameraIntent.resolveActivity(getPackageManager())!=null)
+        //{
+        //    startActivityForResult(cameraIntent, RESULT_TAKE_PHOTO);
+        //}
+
+        Intent chooser = new Intent(Intent.ACTION_CHOOSER);
+        chooser.putExtra(Intent.EXTRA_INTENT, galleryIntent);
+        //chooser.putExtra(Intent.EXTRA_TITLE, getString(R.string.chooseaction));
+        Intent[] intentArray = {cameraIntent};
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+        startActivityForResult(chooser, RESULT_LOAD_IMAGE);
+    }
+
+    private File getFile() {
+        File folder = Environment.getExternalStoragePublicDirectory("/From_camera/imagens");// the file path
+
+        //if it doesn't exist the folder will be created
+        if(!folder.exists())
+        {folder.mkdir();}
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_"+ timeStamp + "_";
+        File image_file = null;
+
+        try {
+            image_file = File.createTempFile(imageFileName,".jpg",folder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mCurrentPhotoPath = image_file.getAbsolutePath();
+        return image_file;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            TextView textViewToChange = (TextView) findViewById(R.id.hello);
-            ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            mGraphicOverlay = findViewById(R.id.graphic_overlay);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // todo use appropriate resultCode in your case
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == FragmentActivity.RESULT_OK) {
+            if (data.getData() != null) {
+                // this case will occur in case of picking image from the Gallery,
+                // but not when taking picture with a camera
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
 
-            imageView.setImageBitmap(imageBitmap);
+                    runTextRecognition(bitmap);
 
-            runTextRecognition(imageBitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
 
-            final List<Classifier.Recognition> results = classifier.recognizeImage(imageBitmap);
+                try {
+                    //help1 = MediaStore.Images.Media.getBitmap(getContentResolver(),file);
+                    //runTextRecognition(help1);
+                    //return;
+                    //imageView.setImageBitmap( thumbnail.extractThumbnail(help1,help1.getWidth(),help1.getHeight()));
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
 
-            textViewToChange.setText(results.toString());
 
-            //textViewToChange.setText("Photo taken successfully!");
+                // this case will occur when taking a picture with a camera
+                Bitmap bitmap = null;
+                Cursor cursor = this.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        new String[]{MediaStore.Images.Media.DATA, MediaStore.Images.Media.DATE_ADDED,
+                                MediaStore.Images.ImageColumns.ORIENTATION}, MediaStore.Images.Media.DATE_ADDED,
+                        null, "date_added DESC");
+                if (cursor != null && cursor.moveToFirst()) {
+                    Uri uri = Uri.parse(cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA)));
+                    String photoPath = uri.toString();
+                    cursor.close();
+                    if (photoPath != null) {
+                        bitmap = BitmapFactory.decodeFile(photoPath);
+                    }
+                }
 
+                if (bitmap == null) {
+                    // for safety reasons you can
+                    // use thumbnail if not retrieved full sized image
+                    bitmap = (Bitmap) data.getExtras().get("data");
+                }
+                runTextRecognition(bitmap);
+                // do whatever you want with the Bitmap ....
+            }
+
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 
     private void initTensorFlowAndLoadModel() {
         executor.execute(new Runnable() {
@@ -102,6 +208,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void runTextRecognition(Bitmap mSelectedImage) {
+        ImageView img = findViewById(R.id.imageView);
+        img.setImageBitmap(mSelectedImage);
+
         FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
         FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
                 .getOnDeviceTextRecognizer();
@@ -132,7 +241,7 @@ public class MainActivity extends AppCompatActivity {
             showToast("No text found");
             return;
         }
-        mGraphicOverlay.clear();
+//        mGraphicOverlay.clear();
         String res = "";
         for (int i = 0; i < blocks.size(); i++) {
             List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
@@ -141,8 +250,8 @@ public class MainActivity extends AppCompatActivity {
                 for (int k = 0; k < elements.size(); k++) {
                     FirebaseVisionText.Element temp = elements.get(k);
                     res += temp.getText() + "\n";
-                    GraphicOverlay.Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
-                    mGraphicOverlay.add(textGraphic);
+              //      GraphicOverlay.Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
+          //          mGraphicOverlay.add(textGraphic);
 
                 }
             }
@@ -157,7 +266,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void requestPermissionForReadExtertalStorage()  {
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.v("Flyer","Permission is granted1");
+        } else {
 
+            Log.v("Flyer","Permission is revoked1");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+        }
+
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.v("Flyer","Permission is granted2");
+            dispatchTakePictureIntent();
+        } else {
+
+            Log.v("Flyer","Permission is revoked2");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
+    }
 
 
 }
