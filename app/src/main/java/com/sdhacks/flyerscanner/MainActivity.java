f9482ad153ce8d753a63e30a9d5cb7a75ed3179c
jbwong05 <jbwong05@gmail.com>
2019-10-26 12:07:13 -0700

    Added picture taking

--- app/src/main/java/com/sdhacks/flyerscanner/MainActivity.java
+++ app/src/main/java/com/sdhacks/flyerscanner/MainActivity.java
@@ -1,13 +1,93 @@
 package com.sdhacks.flyerscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.camerakit.CameraKitView;
+import com.jpegkit.Jpeg;
+import com.jpegkit.JpegImageView;
+
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CameraKitView cameraKitView;
+    private ImageView imageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraKitView = findViewById(R.id.camera);
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(photoOnClickListener);
+    }
+
+    private View.OnClickListener photoOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+                @Override
+                public void onImage(CameraKitView view, final byte[] photo) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try
+                            {
+                                String path = getFilesDir().getAbsolutePath() + "//photo.jpg";
+                                FileOutputStream stream = new FileOutputStream(path);
+                                stream.write(photo);
+                                Bitmap bitmap = BitmapFactory.decodeFile(path);
+                                imageView.setImageBitmap(bitmap);
+                            } catch (Exception e1)
+                            {
+                                e1.printStackTrace();
+                            }
+                        }
+                    }).start();
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        cameraKitView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
