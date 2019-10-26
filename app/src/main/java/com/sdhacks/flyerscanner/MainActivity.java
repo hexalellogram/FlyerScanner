5114fc152b76532588332085682f73f0599289ab
Bryce Tsuyuki <btsuyuki1@gmail.com>
2019-10-26 00:03:28 -0700

    add project files

---
+++ app/src/main/java/com/sdhacks/flyerscanner/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sdhacks.flyerscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
